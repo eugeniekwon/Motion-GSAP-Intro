3c350c59c104715589ba84b8ca5c33a937804477
eugeniekwon <eugenie.kwon@gmail.com>
2020-09-30 04:46:44 +0900

    edits to yellow circle

--- dev/js/yellowCircle.js
+++ dev/js/yellowCircle.js
@@ -1,11 +1,10 @@
 
 import {gsap} from "gsap";  
+import {circleSpeed} from "animationSpeed";  
 
 export function yellowCircleTimeline(){ 
     
     var timeline = gsap.timeline();
-
-    var circleSpeed = 2;
 
     timeline.to("#yellow-circle", {duration:circleSpeed, y:300})
             .to("#yellow-circle", {duration:circleSpeed, x:300});
