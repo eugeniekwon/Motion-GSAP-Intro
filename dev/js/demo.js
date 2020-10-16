9f1244f5f4b2fca1642047d70a7c3928079a7ecf
eugeniekwon <eugenie.kwon@gmail.com>
2020-10-16 20:52:04 +0900

    added svg + motion pt. 1 (toggles working)

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,4 +1,43 @@
 import {gsap} from "gsap";
 
-console.log(gsap);
+//mosquito (night)
+const mosquitoTL = gsap.timeline();
+mosquitoTL
+        // .from("#mosquito", {duration:5, scale: 0.2, ease:"none"})
+        //  .from("#mosquito", {duration:5, x:-10, y:10, rotation:360, ease:"none"}, "-=5")
+        //  .to("#mosquito", {duration:3, x:100, y:200}, "-=3")
+        // //  .to("#mosquito", {duration:3, x:200, y:500, ease:"none"}, "-=3")
+        // // .addPause()
+        // .to("#mosquito", {duration:3, x:100, y:100})
+        
+export function mosquitoAnimation(){
+    return mosquitoTL;
+}
+
+
+//toggle (On)
+const toggleOnTL = gsap.timeline();
+toggleOnTL.to("#circle-toggle", {duration:1.5, x:185})
+        .to("#circle-toggle", {duration:1, fill:"#ffffff"}, "-=0.5")
+        .from("#sun-and-moon", {duration:1, alpha:0}, "-=0.5")
+        .from("#person", {duration:1, alpha:0}, "-=0.5")
+        .from("#mosquito-2", {duration:1.5, alpha:0})
+        
+export function toggleOnAnimation(){
+    return toggleOnTL;
+}
+
+
+//toggle (off)
+const toggleOffTL = gsap.timeline();
+toggleOffTL.to("#person", {duration:1, alpha:0}, "-=0.5")
+            .to("#sun-and-moon", {duration:1, alpha:0}, "-=0.5")
+            .to("#circle-toggle", {duration:1.5, x:0})
+             .to("#circle-toggle", {duration:1, fill:"#272424"}, "-=0.5")
+             .to("#mosquito-2", {duration:1, x: 300, y:-100, alpha:0})
+        
+export function toggleOffAnimation(){
+    return toggleOffTL;
+}
+
 
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,11 @@
-import * as Demo from './demo.js';
+import {gsap} from "gsap";
+import {mosquitoAnimation} from "./demo.js";
+import {toggleOnAnimation} from "./demo.js";
+import {toggleOffAnimation} from "./demo.js";
 
-// quiet codekit
-console.log(Demo);
+const mainTL = gsap.timeline();
+
+mainTL.add(mosquitoAnimation())
+        .add(toggleOnAnimation())
+        // .addPause()
+        .add(toggleOffAnimation())
